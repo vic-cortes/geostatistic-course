140fc40d934879cc46b37cf7019c881e5d9a5636
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-12-19 20:54:10 -0600

    feat: update notebook execution state
    Re-run all cells in main.ipynb to refresh execution counts and outputs with latest data

--- src/config.py
+++ src/config.py
@@ -9,3 +9,4 @@ MEXICO_DIR = MAPS_DIR / "mexico"
 class Config:
     MEXICO_SHAPEFILE = MEXICO_DIR / "00ent.shp"
     IDFC = DATA_DIR / "IDFC_oct2025.csv"
+    SISMO_DATA = DATA_DIR / "Sismos_2000_2025.csv"
